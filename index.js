694615a7b63e57c058f70c05a4d8f4c3887407be
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-28 17:54:28 -0300

    requisito 01

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE || true;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,17 +20,15 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log('Escutando na porta', PORT);
 });
