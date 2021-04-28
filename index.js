25ffde602c76c5c7d7656cabb087fff61f8145f4
Danwhat <daanwhat@gmail.com>
2021-04-28 15:49:56 -0300

    tudão

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT; 
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -29,6 +32,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Escutando na porta 3000');
 });
