a6f608000a5333b3934330bdcb011e67da974ab2
nevesfreire <nando.neves@gmail.com>
2021-04-28 18:31:27 -0300

    backend100

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
