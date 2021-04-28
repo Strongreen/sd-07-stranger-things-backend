6c6aa28782fc94cf9a1deb587af0f12b19580a2d
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-04-28 14:47:22 -0300

    second

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
-
+const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
