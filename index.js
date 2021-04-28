bea0d12d8f742c8a59fa9983ba5c1df52481dd57
nevesfreire <nando.neves@gmail.com>
2021-04-28 16:23:34 -0300

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+require('dotenv').config();
 
 const app = express();
-
+// const port = process.env.PORT || 3000;
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
@@ -16,17 +17,22 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
-
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE) || false;
+let port;
+if (hereIsTheUpsideDown) {
+  port = process.env.PORT || 3003;
+} else {
+  port = process.env.PORT || 3002;
+}
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
-  );
+  );  
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
