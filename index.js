d3a67cf3d0a21ed30faa0c20f7c7a6df8bde74be
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-04-28 17:16:14 -0300

    frist commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const strangerThingsRepository = new StrangerThingsRepository(
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
-//sssss
+
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
