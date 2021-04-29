1a8d6ce21385f7b4db830c59f31b12d77f79345c
PedroMarqDev <PedroMarqDev@gmail.com>
2021-04-29 15:26:45 -0300

    Requisito 1 & 2 Back

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
-const hereIsTheUpsideDown = true;
+const { UPSIDEDOWN_MODE, PORT } = process.env;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
