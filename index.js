d56e2cdf3991b7d61ca3e78689524a0254b57002
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-04-29 13:24:03 -0300

    Conrrigindo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -17,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-const PORT = process.env.PORT || 3002;
+const { PORT } = process.env;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
