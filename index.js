ab7703a693175ec13ac6fd5103871a785e143c65
kramer <kramerscs@gmail.com>
2021-04-28 23:27:05 -0300

    feat: fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -21,7 +21,6 @@ app.use(cors());
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
-  console.log('up', UPSIDEDOWN_MODE)
   const characters = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
