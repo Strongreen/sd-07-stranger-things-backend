c46ca80d3e52fc88c15f75bf8fa670bd7199ea0d
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-04-30 20:16:59 -0300

    configuração .env

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
+
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
