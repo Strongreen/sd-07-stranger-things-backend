03a191a0759926174dbd5556dc5daf0891d1a4af
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-04-30 20:24:13 -0300

    configuração .env upsideDown Mode.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -17,7 +18,6 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
-
 const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
