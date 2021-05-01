d5bb373a147ab9a824dc0253579c424802141b31
moacyrrln <moacyrrln@gmail.com>
2021-05-01 19:59:51 -0300

    incluído o arquivo ecosystem.config.yml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv/config');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -14,9 +15,28 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
+/*
+Backend
+1 - Verifica as variáveis de ambiente
+Altere o backend para utilizar variáveis de ambiente para contrololar os seguintes comportamentos:
+
+A porta que a API escutará, essa variável deve ter, nescessáriamente, o nome PORT.
+
+O modo "upsideDown". Essa variável espera um valor boleano e deverá se chamar UPSIDEDOWN_MODE. Lembre-se que as variáveis de ambinte são strings.
+
+O que será testado:
+
+Se existe a variável de ambiente PORT.
+Se a variável de ambiente UPSIDEDOWN_MODE existe e se ela é um boleano.
+Importante: Para esse projeto, as variáveis de ambiente devem ser definidas em um arquivo .env e o arquivo deve ser enviando no seu PR(Pull Request). ISSO NÃO É UMA PRÁTICA DE MERCADO, o arquivo .env deve ser sempre incluido do .gitignore pois contém informações sensíveis, aqui será enviado apenas por motivo de avaliação.
+*/
+
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
+
+const PORT = process.env.PORT;
+const UPSIDEDOWN_MODE =  process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +47,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT} - ${Date()} - ${UPSIDEDOWN_MODE}`);
 });
