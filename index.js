237d6382a60fded66f6ba1da65ad9cdada654ecd
Marcos Silva <marcos.1998226@gmail.com>
2021-05-06 11:39:24 -0300

    hawkins

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const denv = require('dotenv');
 
+if (process.env.NODE_ENV !== 'production') {
+  denv.config(); // Carrega as variáveis de ambiente em memória
+}
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
-
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
-    req.query, hereIsTheUpsideDown,
+    req.query,
+    hereIsTheUpsideDown,
   );
-
   res.status(200).json(characters);
 });
-
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
