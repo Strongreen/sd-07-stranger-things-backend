b2968e2b0b2c751d7c68be2113a21bebc00e7a0d
muriloPereira006 <muriloedu006@gmail.com>
2021-05-06 14:38:04 -0300

    acho que era esse wait-on que o git reclamou

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const { PORT } = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
