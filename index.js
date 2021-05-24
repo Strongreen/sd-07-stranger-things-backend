d152c150576123d5f924e767a713ccf131c58814
Igor-leao <igorleao.eng@gmail.com>
2021-05-24 18:08:54 -0300

    index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-// const hereIsTheUpsideDown = false;
+const hereIsTheUpsideDown = false;
 const { UPSIDEDOWN_MODE } = process.env;
 
 app.get('/', (req, res) => {
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Linstening to the port ${PORT}`);
 });
