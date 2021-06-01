5b581a8f0d2cd184f276c0cc657f0dd3aa55e71b
Thays Costa <thayscosta3@gmail.com>
2021-06-01 11:24:41 -0300

    Definindo Hawkins

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'false';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
