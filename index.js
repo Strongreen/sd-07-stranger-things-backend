c4b0c9afdb1e7e87101db602869ce6d57a958322
Luiz Mariz <lfsmariz@gmail.com>
2021-06-13 14:14:04 -0300

    update index

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
 
-const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
-const StrangerThingsRepository = require('./data/repository/StrangerThings');
-const StrangerThingsService = require('./services/StrangerThings');
-
+const strangerThingsDataset = require("./data/dataset/stranger-things-characters.json");
+const StrangerThingsRepository = require("./data/repository/StrangerThings");
+const StrangerThingsService = require("./services/StrangerThings");
 const app = express();
-
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+  strangerThingsDataset
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+  strangerThingsRepository
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE === "true"
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
