318717c6533b03496bf16dac13266238c2474d39
Luiz Mariz <lfsmariz@gmail.com>
2021-06-13 14:16:35 -0300

    update lint

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-require("dotenv/config");
-const express = require("express");
-const cors = require("cors");
+require('dotenv/config');
+const express = require('express');
+const cors = require('cors');
 
-const strangerThingsDataset = require("./data/dataset/stranger-things-characters.json");
-const StrangerThingsRepository = require("./data/repository/StrangerThings");
-const StrangerThingsService = require("./services/StrangerThings");
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
 const app = express();
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset
+  strangerThingsDataset,
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository
+  strangerThingsRepository,
 );
 
 app.use(cors());
 
 const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    UPSIDEDOWN_MODE === "true"
+    UPSIDEDOWN_MODE === 'true',
   );
 
   res.status(200).json(characters);
